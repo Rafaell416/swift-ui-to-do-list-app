9e9f96e10d8b92323868790ae641084cb55170c8
Rafael Villarreal <rvillarreal416@gmail.com>
2024-01-16 11:10:04 -0500

    Initial Commit

---
+++ to-do-list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to-do-list
+//
+//  Created by Rafael Villarreal on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
